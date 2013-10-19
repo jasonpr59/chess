a440f67a86388d4143a7c12d152f8cfe08a2902e
Jason Paller-Rzepka <jasonpr@mit.edu>
2013-10-18 23:44:49 -0400

    Use Square in Board. Add some method documentation.
    Using Square, rather than a rank int and a file int, is less
    memory-efficient, but it's cleaner to look at.

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -4,10 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 import exceptions.InvalidMoveException;
+import exceptions.PieceAbilityException;
 
 public class Board {
 
     // board[file - 1][rank - 1] = the piece with specified rank and file
+    // For example, board[1][7] is the piece at b8.
     private final Piece[][] board;
     
     private final Set<Piece> captured;
@@ -22,34 +24,52 @@ public class Board {
         
         // Set up the pawns
         for (int file = 1; file <= 8; file++){
-            placePiece(new Piece(Piece.PieceType.PAWN, Piece.PieceColor.WHITE), file, 2);
-            placePiece(new Piece(Piece.PieceType.PAWN, Piece.PieceColor.BLACK), file, 7);
+            placePiece(new Piece(Piece.PieceType.PAWN, Piece.PieceColor.WHITE),
+                        new Square(file, 2));
+            placePiece(new Piece(Piece.PieceType.PAWN, Piece.PieceColor.BLACK),
+                        new Square(file, 7));
         }
         
         // Set up the pieces
-        placePiece(new Piece(Piece.PieceType.ROOK, Piece.PieceColor.WHITE), 1, 1);
-        placePiece(new Piece(Piece.PieceType.ROOK, Piece.PieceColor.BLACK), 1, 8);
+        placePiece(new Piece(Piece.PieceType.ROOK, Piece.PieceColor.WHITE),
+                   new Square(1, 1));
+        placePiece(new Piece(Piece.PieceType.ROOK, Piece.PieceColor.BLACK),
+                   new Square(1, 8));
         
-        placePiece(new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.WHITE), 2, 1);
-        placePiece(new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.BLACK), 2, 8);
+        placePiece(new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.WHITE),
+                   new Square(2, 1));
+        placePiece(new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.BLACK),
+                   new Square(2, 8));
         
-        placePiece(new Piece(Piece.PieceType.BISHOP, Piece.PieceColor.WHITE), 3, 1);
-        placePiece(new Piece(Piece.PieceType.BISHOP, Piece.PieceColor.BLACK), 3, 8);
+        placePiece(new Piece(Piece.PieceType.BISHOP, Piece.PieceColor.WHITE),
+                   new Square(3, 1));
+        placePiece(new Piece(Piece.PieceType.BISHOP, Piece.PieceColor.BLACK),
+                   new Square(3, 8));
         
-        placePiece(new Piece(Piece.PieceType.QUEEN, Piece.PieceColor.WHITE), 4, 1);
-        placePiece(new Piece(Piece.PieceType.QUEEN, Piece.PieceColor.BLACK), 4, 8);
+        placePiece(new Piece(Piece.PieceType.QUEEN, Piece.PieceColor.WHITE),
+                   new Square(4, 1));
+        placePiece(new Piece(Piece.PieceType.QUEEN, Piece.PieceColor.BLACK),
+                   new Square(4, 8));
         
-        placePiece(new Piece(Piece.PieceType.KING, Piece.PieceColor.WHITE), 5, 1);
-        placePiece(new Piece(Piece.PieceType.KING, Piece.PieceColor.BLACK), 5, 8);
+        placePiece(new Piece(Piece.PieceType.KING, Piece.PieceColor.WHITE),
+                   new Square(5, 1));
+        placePiece(new Piece(Piece.PieceType.KING, Piece.PieceColor.BLACK),
+                   new Square(5, 8));
         
-        placePiece(new Piece(Piece.PieceType.BISHOP, Piece.PieceColor.WHITE), 6, 1);
-        placePiece(new Piece(Piece.PieceType.BISHOP, Piece.PieceColor.BLACK), 6, 8);
+        placePiece(new Piece(Piece.PieceType.BISHOP, Piece.PieceColor.WHITE),
+                   new Square(6, 1));
+        placePiece(new Piece(Piece.PieceType.BISHOP, Piece.PieceColor.BLACK),
+                   new Square(6, 8));
         
-        placePiece(new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.WHITE), 7, 1);
-        placePiece(new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.BLACK), 7, 8);
+        placePiece(new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.WHITE),
+                   new Square(7, 1));
+        placePiece(new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.BLACK),
+                   new Square(7, 8));
         
-        placePiece(new Piece(Piece.PieceType.ROOK, Piece.PieceColor.WHITE), 8, 1);
-        placePiece(new Piece(Piece.PieceType.ROOK, Piece.PieceColor.BLACK), 8, 8);
+        placePiece(new Piece(Piece.PieceType.ROOK, Piece.PieceColor.WHITE),
+                   new Square(8, 1));
+        placePiece(new Piece(Piece.PieceType.ROOK, Piece.PieceColor.BLACK),
+                   new Square(8, 8));
         
         // No captured pieces, yet.
         captured = new HashSet<Piece>();
@@ -66,20 +86,30 @@ public class Board {
         Square start = move.getStart();
         Square end = move.getEnd();
         
-        Piece movingPiece = getPiece(start.getFile(), start.getRank());
+        Piece movingPiece = getPiece(start);
         
         if (!movingPiece.hasMoveAbility(start, end)) {
             throw new PieceAbilityException(movingPiece + " cannot move from " + start + " to " + end);
         }
         
+        // TODO(jasonpr): Finish implementing.
+        
     }
  
-    private void placePiece(Piece piece, int file, int rank){
-        board[file - 1][rank - 1] = piece;
+    /**
+     * Place a piece on a specific square.
+     * 
+     * If another piece is already on that square, it's replaced.
+     */
+    private void placePiece(Piece piece, Square square){
+        board[square.getFile() - 1][square.getRank() - 1] = piece;
     }
     
-    private Piece getPiece(int file, int rank){
-        return board[file - 1][rank - 1];
+    /**
+     * Get the piece at the given square.
+     */
+    private Piece getPiece(Square square){
+        return board[square.getFile() - 1][square.getRank() - 1];
     }
 
     
