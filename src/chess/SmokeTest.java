9146aaf88b95e6d1483b0acbb5e026111ad344b9
Jason Paller-Rzepka <jasonpr@mit.edu>
2013-11-01 18:24:01 -0400

    Add basic smoke test for system so far.
    (This probably doesn't qualify as a true "smoke test."
    Perhaps a better term is "small, nearly random collection
    of test cases.")

---
+++ src/chess/SmokeTest.java
@@ -0,0 +1,32 @@
+package chess;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SmokeTest {
+
+    @Test
+    public void testSmokeTest() {
+        Board b = new Board();
+        Square sq = Square.algebraic("b1");
+
+        // Assert white knight at b1.
+        assertEquals(b.getPiece(sq), new Piece(Piece.PieceType.KNIGHT, Piece.PieceColor.WHITE));
+        
+        // Assert the white knight at b1 can move to c3.
+        Move m1 = new Move(sq, Square.algebraic("c3"));
+        assertTrue(m1.isSane(b));
+        
+        // Assert that the white knight at b1 cannot move to c4
+        Move m2 = new Move(sq, Square.algebraic("c4"));
+        assertFalse(m2.isSane(b));
+        
+        // Assert that the white knight at b1 cannot move to d2.
+        Move m3 = new Move(sq, Square.algebraic("d2"));
+        assertFalse(m3.isSane(b));
+    }
+
+}
