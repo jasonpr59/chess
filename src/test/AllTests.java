39a379b9d3ebc93f0cd76565e684ae4253dec33f
Jason Paller-Rzepka <jasonpr@mit.edu>
2013-12-20 08:57:46 -0500

    Move remaining useful tests from SmokeTest to BoardTest.

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         CastlingTest.class,
-        GameTest.class})
+        GameTest.class,
+        BoardTest.class})
 public class AllTests {
 }
