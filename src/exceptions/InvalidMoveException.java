c8ebf35567d6276c97f35a004a4fff115a52b83d
Jason Paller-Rzepka <jasonpr@mit.edu>
2013-12-21 16:16:13 -0600

    Add UIDs to ChessException and InvalidMoveException.

--- src/exceptions/InvalidMoveException.java
+++ src/exceptions/InvalidMoveException.java
@@ -2,6 +2,8 @@ package exceptions;
 
 public class InvalidMoveException extends ChessException {
 
+    private static final long serialVersionUID = 1L;
+
     public InvalidMoveException() {
         super();
     }
