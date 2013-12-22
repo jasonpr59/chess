90089c622f4eed53075c9ba7355187c901401bef
Jason Paller-Rzepka <jasonpr@mit.edu>
2013-12-21 22:15:40 -0600

    Fix some javadocs.

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,7 +1,11 @@
 package chess;
 
-
+/**
+ * A chess piece.
+ * This class is immutable.
+ */
 public class Piece {
+    /** The type of this piece: pawn, rook, king, etc. */
     public enum PieceType{
         PAWN,
         KNIGHT,
@@ -11,6 +15,7 @@ public class Piece {
         KING
     }
     
+    /** The color of this piece: white or black. */
     public enum PieceColor{
         WHITE,
         BLACK;
@@ -27,11 +32,13 @@ public class Piece {
         }
     }
 
-    // type is final.  Promoting a pawn must involve destroying the pawn
+    // These fields are final (Piece is immutable).
+    // So, promoting a pawn must involve destroying the pawn
     // and creating a new piece of the promoted type.
     private final PieceType type;
     private final PieceColor color;
     
+    /** Construct a new Piece with some type and color. */
     public Piece(PieceType type, PieceColor color){
         this.type = type;
         this.color = color;
