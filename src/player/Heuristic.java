00367aca9a802fcfc2e0aca31a103b5442e67d80
Jason Paller-Rzepka <jasonpr@mit.edu>
2013-12-22 23:58:13 -0600

    Constrain Heuristic to act on Positions.

--- src/player/Heuristic.java
+++ src/player/Heuristic.java
@@ -1,6 +1,6 @@
 package player;
 
-public interface Heuristic<S> {
+public interface Heuristic<P extends Position<P>> {
     // TODO: Figure out a legal alternative to making this a static method.    
-    public float value(S state);
+    public float value(P position);
 }
