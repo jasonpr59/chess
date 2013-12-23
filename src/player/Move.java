80c164bdf3311f274f69f361210d0f972e4af676
Jason Paller-Rzepka <jasonpr@mit.edu>
2013-12-22 23:42:52 -0600

    Remove bad-idea TODO.

--- src/player/Move.java
+++ src/player/Move.java
@@ -1,6 +1,5 @@
 package player;
 
-// TODO: Maybe turn this into an inner class of Position?
 /** A transition from one Position to another. */
 public interface Move<P extends Position<P>> {
     public P result(P position);
