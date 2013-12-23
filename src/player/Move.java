fdeb1baeb823237e37dc957804e3cf678082c00f
Jason Paller-Rzepka <jasonpr@mit.edu>
2013-12-23 00:18:12 -0600

    Add Javadocs to player/Move.java.

--- src/player/Move.java
+++ src/player/Move.java
@@ -2,5 +2,6 @@ package player;
 
 /** A transition from one Position to another. */
 public interface Move<P extends Position<P>> {
+    /** Get the Position resulting form making this Move from some Position. */
     public P result(P position);
 }
