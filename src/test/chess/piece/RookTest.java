858fbf7423acd29b944f66a7221b4e0d949c113f
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-02 12:19:19 -0600

    Add `RookTest` skeleton.

--- src/test/chess/piece/RookTest.java
+++ src/test/chess/piece/RookTest.java
@@ -2,11 +2,77 @@ package test.chess.piece;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import chess.ChessPosition;
+import chess.ChessPositionBuilder;
+import chess.Square;
+import chess.piece.King;
+import chess.piece.Piece;
+import chess.piece.Rook;
+
 public class RookTest {
+
+    private ChessPositionBuilder builder;
+    private ChessPosition position;
+
+    private static Rook WHITE_ROOK = new Rook(Piece.Color.WHITE);
+
+    @Before
+    public void setup() {
+        builder = new ChessPositionBuilder();
+        builder.placePiece(new King(Piece.Color.WHITE), Square.algebraic("b2"));
+        builder.placePiece(new King(Piece.Color.BLACK), Square.algebraic("b7"));
+    }
+
     @Test
-    public void test() {
-        fail("Not yet implemented.");
+    public void testMoveNorthIsSane() {
+        fail();
+    }
+
+    @Test
+    public void testMoveEastIsSane() {
+        fail();
+    }
+
+    @Test
+    public void testMoveSouthIsSane() {
+        fail();
+    }
+
+    @Test
+    public void testMoveWestIsSane() {
+        fail();
+    }
+
+    @Test
+    public void testNonBasicIsInsane() {
+        fail();
+    }
+
+    @Test
+    public void testHopFriendlyPieceIsInsane() {
+        fail();
+    }
+
+    @Test
+    public void testHopEnemyPieceIsInsane() {
+        fail();
+    }
+
+    @Test
+    public void testCaptureIsSane() {
+        fail();
+    }
+
+    @Test
+    public void testFriendlyCaptureIsInsane() {
+        fail();
+    }
+
+    @Test
+    public void testSaneMoves() {
+        fail();
     }
 }
