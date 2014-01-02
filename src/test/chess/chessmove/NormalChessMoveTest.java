26fd38bd318f7656a0470a270917452e390efa2c
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-02 16:50:37 -0600

    Remove redundant, misplaced `testDoublePawnPush` from `NormalChessMoveTest`.
    `PawnTest.testDoubleStepNotFromHomeIsInsane` tests the same functionality,
    and is better placed.  So, we keep that one, and delete this one.

--- src/test/chess/chessmove/NormalChessMoveTest.java
+++ src/test/chess/chessmove/NormalChessMoveTest.java
@@ -1,28 +1,12 @@
 package test.chess.chessmove;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-import chess.NormalChessMove;
-import chess.Game;
-import chess.Square;
-import chess.exceptions.AlgebraicNotationException;
-import chess.exceptions.IllegalMoveException;
-
 public class NormalChessMoveTest {
     @Test
-    public void testDoublePawnPush() throws IllegalMoveException, AlgebraicNotationException{
-        String[] moves = {"e4", "h6"};
-        Game g = Game.fromMoves(moves);
-
-        NormalChessMove illegalDoublePush = new NormalChessMove(Square.algebraic("e4"),
-                                          Square.algebraic("e6"));
-        assertFalse(illegalDoublePush.isLegal(g.getCurrentPosition()));
-    }
-    @Test
     public void test() {
-        fail("Not yet fully implemented.");
+        fail("Not yet implemented.");
     }
 }
