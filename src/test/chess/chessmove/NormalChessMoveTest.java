6a02f92f05479c5386d9ee1cd11b5cb6cd6f7f0b
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-02 17:08:48 -0600

    Add `NormalChessMoveTest` skeleton.

--- src/test/chess/chessmove/NormalChessMoveTest.java
+++ src/test/chess/chessmove/NormalChessMoveTest.java
@@ -5,8 +5,121 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class NormalChessMoveTest {
+
     @Test
-    public void test() {
-        fail("Not yet implemented.");
+    public void testEquality() {
+        fail();
+    }
+
+    @Test
+    public void testConstructors() {
+        fail();
+    }
+
+    @Test
+    public void testGetters() {
+        fail();
+    }
+
+    @Test
+    public void testCapturedSquareNonCapture() {
+        fail();
+    }
+
+    @Test
+    public void testCapturedSquareNormalCapture() {
+        fail();
+    }
+
+    @Test
+    public void testCapturedSquareEnPassant() {
+        fail();
+    }
+
+    @Test
+    public void testEnPassantSquareDoublePushFromHome() {
+        fail();
+    }
+
+    @Test
+    public void testEnPassantSquareSinglePushFromHome() {
+        fail();
+    }
+
+    @Test
+    public void testEnPassantSquareSinglePushToFourthRank() {
+        fail();
+    }
+
+    @Test
+    public void testEnPassantSquareNormalPush() {
+        fail();
+    }
+
+    @Test
+    public void testEnPassantNonPawn() {
+        fail();
+    }
+
+    @Test
+    public void testIsSaneNullPiece() {
+        fail();
+    }
+
+    @Test
+    public void testIsSaneDelegation() {
+        // We do a ton of isSane tests in test.chess.piece.
+        // Here, we just check that NormalChessMove properly
+        // delegates.
+
+        // TODO: Test true result.
+
+        // TODO: Test false result.
+        fail();
+    }
+
+    @Test
+    public void testIsLegalLegalMove() {
+        fail();
+    }
+
+    @Test
+    public void testIsLegalKingAlreadyChecked() {
+        fail();
+    }
+
+    @Test
+    public void testIsLegalPinnedPieceMoved() {
+        fail();
+    }
+
+    @Test
+    public void testPassedThroughBasic() {
+        fail();
+    }
+
+    @Test
+    public void testPassedThroughDiagonal() {
+        fail();
+    }
+
+    @Test
+    public void testResultNormalMove() {
+        fail();
+    }
+
+    @Test
+    public void testResultCapture() {
+        fail();
+    }
+
+    @Test
+    public void testResultEnPassantSquareAffected() {
+        fail();
+    }
+
+    @Test
+    public void testResultCastlingAffected() {
+        fail();
     }
 }
