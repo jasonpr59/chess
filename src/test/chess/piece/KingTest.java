b66e4e99ded30971061874c3485368579ed391a3
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-02 13:34:42 -0600

    Add `KingTest` skeleton.

--- src/test/chess/piece/KingTest.java
+++ src/test/chess/piece/KingTest.java
@@ -6,7 +6,42 @@ import org.junit.Test;
 
 public class KingTest {
     @Test
-    public void test() {
-        fail("Not yet implemented.");
+    public void testNonCaptureIsSane() {
+        fail();
+    }
+
+    @Test
+    public void testCaptureIsSane() {
+        fail();
+    }
+
+    @Test
+    public void testFriendlyCaptureIsInsane() {
+        fail();
+    }
+
+    @Test
+    public void testMoveTooFarIsInsane() {
+        fail();
+    }
+
+    @Test
+    public void testCastlingIsSane() {
+        fail();
+    }
+
+    @Test
+    public void testCastlingWhenObstructedIsInsane() {
+        fail();
+    }
+
+    @Test
+    public void testCastlingWhenNoLongerAllowedIsInsane() {
+        fail();
+    }
+
+    @Test
+    public void testSaneMoves() {
+        fail();
     }
 }
