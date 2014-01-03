a6d0607c144b6d2c554985f0dbbe07e5801723f4
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-02 22:23:19 -0600

    Add `CastlingMoveTest` skeleton.

--- src/test/chess/chessmove/CastlingMoveTest.java
+++ src/test/chess/chessmove/CastlingMoveTest.java
@@ -5,8 +5,109 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class CastlingMoveTest {
+
     @Test
-    public void test() {
-        fail("Not yet implemented.");
+    public void testEquality() {
+        fail();
     }
+
+    @Test
+    public void testGetters() {
+        // Test getColor.
+
+        // Test getSide.
+
+        fail();
+    }
+
+    @Test
+    public void testCalculatedGetters() {
+        // Test getStart and getEnd.
+
+        // Test getRookStart and getRookEnd.
+
+        fail();
+    }
+
+    @Test
+    public void testFromNormalMove() {
+        fail();
+    }
+
+    @Test
+    public void testEnPassantSquare() {
+        fail();
+    }
+
+    @Test
+    public void testCapturedSquare() {
+        fail();
+    }
+
+    @Test
+    public void testIsSaneNoMovingPiece() {
+        fail();
+    }
+
+    @Test
+    public void testIsSaneDelegation() {
+
+        // Result is true.
+
+        // Result is false.
+
+        fail();
+    }
+
+    @Test
+    public void testIsLegalLegalMove() {
+        fail();
+    }
+
+    @Test
+    public void testCastleOutOfCheckIsIllegal() {
+        fail();
+    }
+
+    @Test
+    public void testCastleThroughCheckIsIllegal() {
+        fail();
+    }
+
+    @Test
+    public void testCastleIntoCheckIsIllegal() {
+        fail();
+    }
+
+    @Test
+    public void testInsaneCastleIsIllegal() {
+        fail();
+    }
+
+    @Test
+    public void testPassedThrough() {
+        fail();
+    }
+
+    @Test
+    public void testWhiteKingCastleResult() {
+        fail();
+    }
+
+    @Test
+    public void testWhiteQueenCastleResult() {
+        fail();
+    }
+
+    @Test
+    public void testBlackKingCastleResult() {
+        fail();
+    }
+
+    @Test
+    public void testBlackQueenCastleResult() {
+        fail();
+    }
+
+
 }
