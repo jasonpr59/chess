13497e6bd82a4813cd4fd3a78a8be60017c511bb
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-03 16:55:37 -0600

    Fully implement `AlgebraicNotation.parse`.

--- src/chess/AlgebraicNotation.java
+++ src/chess/AlgebraicNotation.java
@@ -1,5 +1,10 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import chess.exceptions.AlgebraicNotationException;
 import chess.piece.Bishop;
 import chess.piece.King;
@@ -14,6 +19,19 @@ import chess.piece.Rook;
  * See http://en.wikipedia.org/wiki/Algebraic_notation_(chess).
  */
 public class AlgebraicNotation {
+
+
+    private final static String typePattern = "(N|B|R|Q|K)?";
+    private final static String cluePattern = "([a-h]?[1-8]?)";
+    private final static String capturesPattern = "(x?)";
+    private final static String endPattern = "([a-h][1-8])";
+    private final static String promotionPattern = "(?:=(N|B|R|Q))?";
+    private final static String checkPattern = "(\\+|#)?";
+
+    private final static Pattern algebraic =
+            Pattern.compile(typePattern + cluePattern + capturesPattern +
+                            endPattern + promotionPattern + checkPattern);
+
     /**
      * Create a ChessMove from its algebraic representation.
      *
@@ -30,8 +48,6 @@ public class AlgebraicNotation {
      *      as a move on the given ChessPosition.
      */
     public static ChessMove parse(String alg, ChessPosition position) throws AlgebraicNotationException {
-        // TODO: Refactor the heck out of this mess!  Or, at least, comment the heck out of it.
-
         if (alg.equals("O-O")) {
             return new CastlingMove(CastlingMove.Side.KINGSIDE, position.getToMoveColor());
         }
@@ -40,65 +56,89 @@ public class AlgebraicNotation {
             return new CastlingMove(CastlingMove.Side.QUEENSIDE, position.getToMoveColor());
         }
 
-        // TODO: Implement promotion.
+        Matcher matcher = algebraic.matcher(alg);
 
-        final String endStr;
-        final boolean captures;
+        if (!matcher.matches()) {
+            throw new AlgebraicNotationException("Could not match move `" + alg +
+                                                 "` to Algebraic Notation template");
+        }
 
-        int startFront = 1;
-        int startBack;
+        String type = matcher.group(1);
+        String clue = matcher.group(2);
+        String capturesSignal = matcher.group(3);
+        String endAlgebraic = matcher.group(4);
+        String promotion = matcher.group(5);
+        String check = matcher.group(6);
 
-        // Get piece type.
-        char typeChar = alg.charAt(0);
-        Piece piece;
         Piece.Color color = position.getToMoveColor();
-        if (typeChar == 'N') {
-            piece = new Knight(color);
-        } else if (typeChar == 'B') {
-            piece = new Bishop(color);
-        } else if (typeChar == 'R') {
-            piece = new Rook(color);
-        } else if (typeChar == 'Q') {
-            piece = new Queen(color);
-        } else if (typeChar == 'K') {
-            piece = new King(color);
-        } else {
-            piece = new Pawn(color);
-            startFront = 0;
-        }
 
-        // Get end square.
-        endStr = alg.substring(alg.length() - 2, alg.length());
-        Square end = Square.algebraic(endStr);
+        Piece piece = piece(type, color);
 
-        // Get captures
-        int capturesIndex = alg.length() - 3;
-        if (capturesIndex >= 0 && alg.charAt(capturesIndex) == 'x') {
-            captures = true;
-            startBack = capturesIndex;
-        } else {
-            captures = false;
-            startBack = capturesIndex + 1;
-        }
+        Square end = Square.algebraic(endAlgebraic);
+
+        boolean promotes = (promotion != null);
 
         Square start;
-        int startLen = startBack - startFront;
-        if (startLen == 2){
-            // Have the square.
-            start = Square.algebraic(alg.substring(startFront, startBack));
-        } else if (startLen == 1) {
-            char clue = alg.charAt(startFront);
-            start = startFromClue(piece.getClass(), clue, end, position);
+        if (clue.equals("")) {
+            start = start(piece.getClass(), end, position, promotes);
         } else {
-            start = start(piece.getClass(), end, position);
+            start = startFromClue(piece.getClass(), clue, end, position, promotes);
         }
 
-        NormalChessMove move = new NormalChessMove(start, end);
+        ChessMove move;
 
-        // TODO(jasonpr): Do some more sanity checks
-        if (captures == (move.capturedSquare(position) == null)) {
-            throw new AlgebraicNotationException("A capture was indicated with 'x', but " +
-                                                 "the indicated move doesn't perform a capture.");
+        NormalChessMove normalMove = new NormalChessMove(start, end);
+        if (!promotes) {
+            move = normalMove;
+        } else if ("N".equals(promotion)) {
+            move = new PromotionMove(normalMove, new Knight(color));
+        } else if ("B".equals(promotion)) {
+            move = new PromotionMove(normalMove, new Bishop(color));
+        } else if ("R".equals(promotion)) {
+            move = new PromotionMove(normalMove, new Rook(color));
+        } else if ("Q".equals(promotion)) {
+            move = new PromotionMove(normalMove, new Queen(color));
+        } else {
+            throw new AlgebraicNotationException("Illegal promotion type `" + promotion + "`.");
+        }
+
+        boolean captures;
+        if ("x".equals(capturesSignal)) {
+            captures = true;
+        } else if ("".equals(capturesSignal)) {
+            captures = false;
+        } else {
+            throw new AlgebraicNotationException(
+                    "Illegal captures signal `" + capturesSignal + "`.");
+        }
+
+        boolean moveReallyCaptures = (move.capturedSquare(position) != null);
+        if (moveReallyCaptures && !captures) {
+            throw new AlgebraicNotationException("Capturing move has no 'x'.");
+        } else if (!moveReallyCaptures && captures) {
+            throw new AlgebraicNotationException("Non-capturing move has an 'x'.");
+        }
+
+        ChessPosition result = move.result(position);
+
+        boolean isReallyCheck = result.checked(result.getToMoveColor());
+        boolean isReallyCheckMate = isReallyCheck && (result.moves().size() == 0);
+
+        if (check == null) {
+            // Assert no check.
+            if (isReallyCheck) {
+                throw new AlgebraicNotationException("Checking move has no '+' or '#'.");
+            }
+        } else if ("+".equals(check)) {
+            // Assert is check.
+            if (!isReallyCheck) {
+                throw new AlgebraicNotationException("Non-checking move has a '+'.");
+            }
+        } else if ("#".equals(check)) {
+            // Assert is mate
+            if (!isReallyCheckMate) {
+                throw new AlgebraicNotationException("Non-checkmating move has a '#'.");
+            }
         }
 
         return move;
@@ -111,24 +151,35 @@ public class AlgebraicNotation {
      * @param position The position from which the move is to be made.
      * @throws AlgebraicNotationException if there is no possible starting square.
      */
-    private static Square start(Class<?> type, Square end, ChessPosition position) throws AlgebraicNotationException {
-        // TODO: Make more efficient.
-        // TODO: Check uniqueness of start square (i.e. that no disambiguation
-        // was necessary).
-        return startFromSquares(type, Square.ALL, end, position);
+    private static Square start(Class<?> type, Square end, ChessPosition position,
+                                boolean promotes) throws AlgebraicNotationException {
+        return startFromSquares(type, Square.ALL, end, position, promotes);
     }
 
     /**
-     * Get the only possible starting square for a move, given some rank or file clue.
+     * Get the only possible starting square for a move, given some rank and/or file clue.
      * @param type The type of the moving piece.
-     * @param clue The rank or file from which to select the starting square,
-     *      in ['a', 'h'] or ['1', '8'].  (This comes from, say, the 'b' in "Rbe5".)
+     * @param clue The rank and/or file from which to select the starting square,
+     *      in ['a', 'h'] or ['1', '8'].  (This comes from, say, the 'b' in "Rbe5",
+     *      or the '8' in "R8e5", or the "c7" in "Nc7d5".
      * @param end The move's ending square.
      * @param position The position from which the move is to be made.
      * @throws AlgebraicNotationException if there is no possible starting square.
      */
-    private static Square startFromClue(Class<?> type, char clue, Square end, ChessPosition position) throws AlgebraicNotationException {
-        return startFromSquares(type, Square.line(clue), end, position);
+    private static Square startFromClue(Class<?> type, String clue, Square end,
+                                        ChessPosition position, boolean promotes)
+                                                throws AlgebraicNotationException {
+        Collection<Square> candidateStarts = new ArrayList<Square>();
+        if (clue.length() == 1) {
+            char clueChar = clue.charAt(0);
+            candidateStarts.addAll(Square.line(clueChar));
+        } else if (clue.length() == 2) {
+            candidateStarts.add(Square.algebraic(clue));
+        } else {
+            throw new IllegalArgumentException("Clue must have length 1 or 2.");
+        }
+        return startFromSquares(type, candidateStarts, end, position, promotes);
+
     }
 
     /**
@@ -142,9 +193,12 @@ public class AlgebraicNotation {
      *      square in candidateSqarues.
      */
     private static Square startFromSquares(Class<?> type, Iterable<Square> candidateStarts,
-                                           Square end, ChessPosition position) throws AlgebraicNotationException {
+                                           Square end, ChessPosition position, boolean promotes)
+                                                   throws AlgebraicNotationException {
         Piece movingPiece;
         ChessMove candidateMove;
+
+        ArrayList<Square> possibleStarts = new ArrayList<Square>();
         for (Square start : candidateStarts) {
             if (start.equals(end)) {
                 continue;
@@ -154,13 +208,45 @@ public class AlgebraicNotation {
                 movingPiece.getColor() != position.getToMoveColor()) {
                 continue;
             }
-            candidateMove = new NormalChessMove(start, end);
+
+            NormalChessMove normalMove = new NormalChessMove(start, end);
+            if (promotes) {
+                // The promotedType doesn't matter, here.  We just use a queen.
+                candidateMove =
+                        new PromotionMove(normalMove, new Queen(position.getToMoveColor()));
+            } else {
+                candidateMove = normalMove;
+            }
 
             if (candidateMove.isLegal(position)) {
-                return start;
+                possibleStarts.add(start);
             }
         }
-        // There is no such start square.
-        throw new AlgebraicNotationException();
+        if (possibleStarts.size() == 1) {
+            return possibleStarts.get(0);
+        } else if (possibleStarts.size() == 0) {
+            throw new AlgebraicNotationException("There is no starting square for that move.");
+        } else {
+            throw new AlgebraicNotationException(
+                    "There are multiple possible starting squares for that move!");
+        }
+    }
+
+    private static Piece piece(String type, Piece.Color color) throws AlgebraicNotationException {
+        if (type == null) {
+            return new Pawn(color);
+        } else if (type.equals("N")) {
+            return new Knight(color);
+        } else if (type.equals("B")) {
+            return new Bishop(color);
+        } else if (type.equals("R")) {
+            return new Rook(color);
+        } else if (type.equals("Q")) {
+            return new Queen(color);
+        } else if (type.equals("K")) {
+            return new King(color);
+        } else {
+            throw new AlgebraicNotationException("Illegal piece type: `" + type + "`.");
+        }
     }
 }
