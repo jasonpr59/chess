6518c492d4da161662b600d42fc18909d787bbd9
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-03 00:15:46 -0600

    Add `AlgebraicNotationTest` skeleton.

--- src/test/chess/AlgebraicNotationTest.java
+++ src/test/chess/AlgebraicNotationTest.java
@@ -1,14 +1,126 @@
 package test.chess;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class AlgebraicNotationTest {
 
+    // TODO: Determine whether to require AlgebraicNotationException
+    // on over-specific clues.
+
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testPawnMove() {
+        fail();
     }
 
+    @Test
+    public void testKnightMove() {
+        fail();
+    }
+
+    @Test
+    public void testBishopMove() {
+        fail();
+    }
+
+    @Test
+    public void testRookMove() {
+        fail();
+    }
+
+    @Test
+    public void testQueenMove() {
+        fail();
+    }
+
+    @Test
+    public void testKingMove() {
+        fail();
+    }
+
+    @Test
+    public void testNoMatchingPiece() {
+        fail();
+    }
+
+    @Test
+    public void testMultipleMatchingPieces() {
+        fail();
+    }
+
+    @Test
+    public void testFileClue() {
+        fail();
+    }
+
+    @Test
+    public void testRankClue() {
+        fail();
+    }
+
+    @Test
+    public void testSquareClue() {
+        fail();
+    }
+
+    @Test
+    public void testSquareClueWrongPiece() {
+        fail();
+    }
+
+    @Test
+    public void testTakesCorrectlyShown() {
+        fail();
+    }
+
+    @Test
+    public void testTakesMistakenlyShown() {
+        fail();
+    }
+
+    @Test
+    public void testTakesCorrectlyOmitted() {
+        fail();
+    }
+
+    @Test
+    public void testTakesMistakenlyOmitted() {
+        fail();
+    }
+
+    @Test
+    public void testCheckCorrectlyShown() {
+        fail();
+    }
+
+    @Test
+    public void testCheckMistakenlyShown() {
+        fail();
+    }
+
+    @Test
+    public void testCheckCorrectlyOmitted() {
+        fail();
+    }
+
+    @Test
+    public void testCheckMistakenlyOmitted() {
+        fail();
+    }
+
+    @Test
+    public void testPromotionCorrectlyShown() {
+        fail();
+    }
+
+    @Test
+    public void testPromotionMistakenlyShown() {
+        fail();
+    }
+
+    @Test
+    public void testPromotionMistakenlyOmitted() {
+        fail();
+    }
 }
