4754e2575388d7a88efe235ccc0c853769c1403b
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-04 13:14:45 -0600

    Add `Score`.
    Implmenting subclasses will follow in upcoming commits.

---
+++ src/player/Score.java
@@ -0,0 +1,14 @@
+package player;
+
+/**
+ * The score of a Position.
+ * Positive scores mean the maximizing player is favored.
+ * Negative scores mean the minimizing player is favored.
+ */
+public abstract class Score {
+    public abstract float getValue();
+    public abstract boolean greaterThan(Score s);
+    public boolean lessThan(Score s) {
+        return !greaterThan(s) && !equals(s);
+    }
+}
