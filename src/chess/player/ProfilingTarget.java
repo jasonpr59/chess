bebb0d8009c403fe8da7c0ffcd56c9ef3f7e0227
Jason Paller-Rzepka <jasonpr@mit.edu>
2014-01-05 00:51:07 -0600

    Make ProfilingTarget state how long it took to run.
    We're moving in the direction of having some quantitative way
    to measure how fast this thing is.  In turn, that will give us
    an objective way of determining if our speed "improvements"
    are actually improvements.

--- src/chess/player/ProfilingTarget.java
+++ src/chess/player/ProfilingTarget.java
@@ -8,12 +8,19 @@ import chess.ChessPositionBuilder;
 
 public class ProfilingTarget {
 
+    private static final int RUNS = 20;
+
     public static void main(String[] args) {
         // Just run some AlphaBeta, so the profiler can do its thing.
         Heuristic<ChessPosition> heuristic = new BoardPieceValueHeuristic();
         Decider<ChessPosition> decider = new AlphaBeta<ChessPosition>(heuristic);
-        decider.bestDecision(new ChessPositionBuilder().setupNewGame().build(), 3);
-        System.out.println("Done.");
-    }
 
+        long startTime = System.currentTimeMillis();
+        for (int run = 0; run < RUNS; run++){
+            decider.bestDecision(new ChessPositionBuilder().setupNewGame().build(), 4);
+        }
+        long endTime = System.currentTimeMillis();
+        long runTime = endTime- startTime;
+        System.out.println("Took " + runTime + "ms for " + RUNS + " runs.");
+    }
 }
